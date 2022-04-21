da1d960e3bdbd6e495fc68135bedb98798e91449
Sergey <enotagressor@gmail.com>
2022-04-22 02:41:42 +0700

    3

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,12 @@ public class Main {
 
         // Zadanie 3
 
-
+        int year = 2000;
+        if (year%4 == 0 && year % 100 != 0 || year%400 == 0){
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
 
         // Zadanie 4
 
